3efea26958cc9dab24b068fb6270cfdf80d166bb
Jamie Ohakwe <jamieohakwe@gmail.com>
2024-05-19 04:16:53 +0100

    updating

---
+++ src/components/GetStarted/GetStarted.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
+
+const GetStarted = () => {
+  return (
+    <>
+        <div className='bg-white w-[591px] h-[148px] flex gap-[15px] items-center p-6 rounded-xl'>
+            <div className='border w-[163px] h-[100px]'></div>
+            <div className='flex flex-col'>
+                <span className='font-bold'>Get Started as a Vendor</span>
+                <span className='font-xs text-sm text-gray-500'>Seamless Convenience: Swift, Free Delivery to Your Door!</span>
+            </div>
+                <div className='bg-gray-200 rounded-xl w-[40px] h-[40px] flex items-center justify-center'>
+                <FaArrowRight />
+                </div>
+        </div>
+    </>
+  )
+}
+
+export default GetStarted
