c3097fceb3ec5f9326687790825e85ee8e2e7e8e
Jamie Ohakwe <jamieohakwe@gmail.com>
2024-08-10 15:57:53 +0100

    fixed test

--- src/components/NavBar/NavBarMenu/NavMenu.tsx
+++ src/components/NavBar/NavBarMenu/NavMenu.tsx
@@ -1,31 +1,33 @@
-
-
-
-
-
-
 export const middleMenu: Menu[] = [
-    {id: 1, menu: 'Products', dropDown: true, dropMenu: [
-        {menu: 'Cake', menuCategory: ['Birthday', 'Wedding', 'Corporate', 'Events']} ,
-        {menu: 'Drinks'},
-        {menu: 'Venues'},
-        {menu: 'Caterers'},
-        {menu: 'Djs'},
-        {menu: 'MCs'},
-        {menu: 'Photographers'},
-        {menu: 'Party Decorators'},
-        {menu: 'Vendor'}
-    ]},
-    {id: 2, menu: 'Categories', dropDown: true},
-    {id: 3, menu: 'FAQs',dropDown: false},
-    {id: 4, menu: 'Contact', dropDown: false},
-    {id: 5, menu: 'Join us', dropDown: true},
-]
+	{
+		id: 1,
+		menu: "Products",
+		dropDown: true,
+		dropMenu: [
+			{
+				menu: "Cake",
+				menuCategory: ["Birthday", "Wedding", "Corporate", "Events"],
+			},
+			{ menu: "Drinks" },
+			{ menu: "Venues" },
+			{ menu: "Caterers" },
+			{ menu: "Djs" },
+			{ menu: "MCs" },
+			{ menu: "Photographers" },
+			{ menu: "Party Decorators" },
+			{ menu: "Vendor" },
+		],
+	},
+	{ id: 2, menu: "Categories", dropDown: true },
+	{ id: 3, menu: "FAQs", dropDown: false },
+	{ id: 4, menu: "Contact", dropDown: false },
+	{ id: 5, menu: "Join us", dropDown: true },
+];
 
 type Menu = {
-    id: number;
-    menu: string;
-    dropDown: boolean;
-    dropMenu?: any[];
-    // dropMenu?: (string | string[] | Record<string, string>)[];
-}
+	id: number;
+	menu: string;
+	dropDown: boolean;
+	dropMenu?: any[];
+	// dropMenu?: (string | string[] | Record<string, string>)[];
+};
